954d67af13f6d62b18d204cc1c77063d674c8c98
Paul Fedory <paul.fedory@gmail.com>
2017-02-21 00:39:42 -0500

    Revert "Add Grid parent"
    This reverts commit ca7f266d2c1007c1da915e4232216deabd0207fd.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Grid, Header } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 import './App.css';
 import ClapForm from './ClapForm.js'
 import Emoji from './Emoji.js'
@@ -10,10 +10,8 @@ class App extends Component {
     var random2 = Emoji.random();
     return (
       <Container text={ true } textAlign='center' className='main'>
-        <Grid>
-          <Header size='huge' as='h1' color='red'>CLAP {random1} THAT {random2}</Header>
-          <ClapForm />
-        </Grid>
+        <Header size='huge' as='h1' color='red'>CLAP {random1} THAT {random2}</Header>
+        <ClapForm />
       </Container>
     );
   }
